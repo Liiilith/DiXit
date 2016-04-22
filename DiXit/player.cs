99e03ea83ebee5175e77ec28461d16a7d6745c39
Tom <tom_rycerz@wp.pl>
2016-04-23 01:19:40 +0200

    pa6

---
+++ DiXit/player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiXit
+{
+
+    enum playerType
+    {
+        challanger, guesser, unsign
+    };
+
+
+
+    class Player
+    {
+
+
+        playerType type;
+
+        public void setTyp (playerType a)      // procedura ustawiajaca gracza 
+
+        { type = a; }
+
+
+        public playerType getType()           // zwraca typ gracza
+        {
+            return type;
+        } 
+
+
+
+
+    }
+}
