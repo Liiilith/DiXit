a6e6f055192f2ccfc7297dc105c7c7a574fc320c
Your Name <you@example.com>
2016-04-23 01:16:08 +0200

    pa5

--- DiXit/Form2.Designer.cs
+++ DiXit/Form2.Designer.cs
@@ -91,6 +91,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.label1.Cursor = System.Windows.Forms.Cursors.Default;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label1.Location = new System.Drawing.Point(184, 80);
