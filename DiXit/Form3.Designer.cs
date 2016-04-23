a45680a69ac33897e21f8c401753288dff01daef
Tom <tom_rycerz@wp.pl>
2016-04-23 23:56:07 +0200

    zmiana

--- DiXit/Form3.Designer.cs
+++ DiXit/Form3.Designer.cs
@@ -35,9 +35,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(53, 38);
+            this.button1.Location = new System.Drawing.Point(134, 364);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(216, 63);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -45,9 +45,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(187, 37);
+            this.button2.Location = new System.Drawing.Point(255, 121);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(190, 131);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
@@ -55,9 +55,9 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(107, 127);
+            this.button3.Location = new System.Drawing.Point(34, 121);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(191, 131);
             this.button3.TabIndex = 2;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
