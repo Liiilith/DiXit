cfcc7220ae437c8257a45754bf967f04ca604f66
Your Name <you@example.com>
2016-04-23 17:02:32 +0200

    client srv updae

--- DiXit/Client.cs
+++ DiXit/Client.cs
@@ -8,5 +8,6 @@ namespace DiXit
 {
     class Client
     {
+        playersData playersList;
     }
 }
--- DiXit/Server.cs
+++ DiXit/Server.cs
@@ -8,5 +8,6 @@ namespace DiXit
 {
     class Server
     {
+        playersData playersList;
     }
 }
